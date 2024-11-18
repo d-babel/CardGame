d7d603090f2a83e92aa2b0dcdfeeaaa57c4b4161
d-babel <drew.babel@menloschool.org>
2024-11-18 15:13:24 -0800

    Add community card dealing functionality

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,8 @@ public class Game {
     private Deck deck;
     private Player player1;
     private Player player2;
+    private Card[] communityCards;
+    private int communityCardCount;
 
     public Game() {
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
@@ -11,6 +13,9 @@ public class Game {
         int[] values = {2, 3, 4, 5, 6 , 7, 8, 9, 10, 10, 10, 10, 11};
 
         deck = new Deck(ranks, suits, values);
+        communityCards = new Card[5];
+        communityCardCount = 0;
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter Player 1's name: ");
@@ -34,6 +39,41 @@ public class Game {
 
         System.out.println(player1);
         System.out.println(player2);
+
+        dealFlop();
+        System.out.println("Flop: " + getCommunityCardsString());
+
+        dealTurn();
+        System.out.println("Turn: " + getCommunityCardsString());
+
+        dealRiver();
+        System.out.println("River: " + getCommunityCardsString());
+
+    }
+
+    public void dealFlop() {
+        for (int i = 0; i < 3; i++) {
+            communityCards[communityCardCount++] = deck.deal();
+        }
+    }
+
+    public void dealTurn() {
+        communityCards[communityCardCount++] = deck.deal();
+    }
+
+    public void dealRiver() {
+        communityCards[communityCardCount++] = deck.deal();
+    }
+
+    public String getCommunityCardsString() {
+        String result = "";
+        for (int i = 0; i < communityCardCount; i++){
+            result += communityCards[i];
+            if (i < communityCardCount - 1) {
+                result += ", ";
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
